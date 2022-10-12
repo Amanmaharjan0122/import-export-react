2c683c14e24b815a564fb8a2368fba1613b838a6
Amanmaharjan0122 <amanmaharjan6201@gmail.com>
2022-10-12 23:12:37 +0545

    import export react

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+const name = "aman";
+export default name;
+
+const lname = "maharjan"
+
+function fName() {
+    let Fname = "aman maharjan"; return Fname
+}
+function fevGame() {
+    let Fevgame = "aman maharjan"; return Fevgame
+}
+export {lname, fName, fevGame};
